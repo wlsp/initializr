144fd1af0332f6ce28cabc7a06494e7a3660b3f6
wlsp <wlsp@outlook.de>
2025-04-15 10:57:50 +0200

    -initial push

--- src/main/java/wlsp/tech/initializr/InitializrApplication.java
+++ src/main/java/wlsp/tech/initializr/InitializrApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InitializrApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InitializrApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InitializrApplication.class, args);
+    }
 
 }
