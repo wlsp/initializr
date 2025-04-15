543854fcea065df3361cb8188cf854ebf1734595
wlsp <wlsp@outlook.de>
2025-04-15 10:53:09 +0200

    Initial commit

---
+++ src/test/java/wlsp/tech/initializr/InitializrApplicationTests.java
@@ -0,0 +1,13 @@
+package wlsp.tech.initializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
